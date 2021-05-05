534fa3a153a0e85b50aedac1c360d3d307e1db34
odraudep <odraudep.cont@gmail.com>
2021-05-05 20:03:19 -0300

    client header appereance done

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,55 @@
 <template>
-  <h1>Todo App</h1>
+  <div>
+    <Navbar />
+    <h1>Todo App</h1>
+  </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
+
 export default {
-  name: "App"
-}
+  name: "App",
+  components: { Navbar }
+};
 </script>
 
 <style>
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+:root {
+  --body-clr: #F5F5F5;
+  --txt-clr: #222;
+  --clr1: #069;
+
+  /* width */
+  --menu-width: 20rem;
+
+  /* TRANSITIOIN */
+  --duration: .3s;
+}
+html.alt-color-mode:root {
+  --body-clr: #222;
+  --txt-clr: #EFEFEF;
+  --clr1: #069;
+}
 body {
-  font-family: 'Roboto', sans-serif;
+  overflow-x: hidden;
+  background-color: var(--body-clr);
+  font-family: "Roboto", sans-serif;
+  color: var(--txt-clr);
+  transition: background-color var(--duration), transform var(--duration);
+}
+body.menu-open {
+  transform: translate3d(calc(var(--menu-width) * -1), 0, 0);
+}
+.container {
+  max-width: 1300px;
+  margin: 0 auto;
 }
 </style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,207 @@
+<template>
+  <header class="header">
+    <nav class="header__nav container">
+      <a href="/">
+        <img src="@/assets/brand.png" alt="Brand" class="header__brand">
+      </a>
+
+      <button class="header__toggler" @click="toggleMenu($event)" target="#menu">
+        <i class="fas fa-bars"></i>
+      </button>
+
+      <ul class="header__menu" id="menu">
+        <li class="header__item">
+          <a href="/" class="header__link actived">Home</a>
+        </li>
+
+        <li class="header__item">
+          <a href="/" class="header__link">Favorites</a>
+        </li>
+
+        <li class="header__item">
+          <a href="/" class="header__link">Manage</a>
+        </li>
+
+        <li class="header__item">
+          <button
+            type="button" class="toggle-dark"
+            @click="toggleDark"
+          >
+            <i class="fas fa-adjust"></i>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  methods: {
+    toggleMenu(e) {
+      const toggler = e.target.classList.contains("header__toggler")
+        ? e.target
+        : e.target.parentElement;
+      const target = toggler.getAttribute("target");
+      const menu = document.querySelector(target);
+
+      menu.classList.toggle("open");
+      document.body.classList.toggle("menu-open");
+    },
+    toggleDark() {
+      document.querySelector("html").classList.toggle("alt-color-mode");
+    }
+  },
+  mounted() {
+    addEventListener("resize", toggleMenuBehavior);
+
+    const dom = {
+      queryStyle: "style#menuBehavior"
+    };
+
+    function toggleMenuBehavior() {
+      const menu = document.querySelector(".header__menu");
+
+      if (innerWidth > 768) {
+        if (menu.classList.contains("open"))
+          menu.classList.remove("open");
+
+        if (document.body.classList.contains("menu-open"))
+          document.body.classList.remove("menu-open");
+
+        return;
+      }
+
+      const toggler = document.querySelector(".header__toggler");
+
+      const menuWidth = menu.getBoundingClientRect().width;
+      const togglerFromRight = innerWidth - toggler.getBoundingClientRect().right + toggler.getBoundingClientRect().width;
+
+      if (innerWidth > menuWidth + togglerFromRight && !menu.classList.contains("open")) {
+        if (document.querySelector(dom.queryStyle))
+          document.querySelector(dom.queryStyle).remove();
+
+        return;
+      }
+
+      if (document.querySelector(dom.queryStyle) || menu.classList.contains("open")) return;
+
+      // STYLE ELEMENT
+      const style = document.createElement("style");
+
+      style.textContent = `
+        .header__menu.open {
+          transform: translate3d(calc(var(--menu-width) * -1), 0, 0);
+        }
+        body.menu-open {
+          transform: translate3d(0, 0, 0);
+        }
+      `;
+      style.id = "menuBehavior";
+
+      document.body.appendChild(style);
+    }
+
+    toggleMenuBehavior();
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  padding: .5rem 5rem;
+  box-shadow: 0 .1rem .5rem rgb(25 25 25 / .15);
+  background-color: var(--body-clr);
+  transition: background-color var(--duration);
+}
+.header__nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.header__brand {
+  max-width: 5rem;
+  max-height: 5rem;
+}
+.header__toggler,
+.toggle-dark {
+  cursor: pointer;
+  outline: none;
+  border: none;
+  background: none;
+}
+.header__toggler {
+  display: none;
+  font-size: 2rem;
+}
+.header__menu {
+  list-style: none;
+  display: flex;
+  align-items: center;
+}
+.header__item {
+  margin: 0 1.5em;
+}
+.header__link {
+  text-decoration: none;
+  text-transform: uppercase;
+  font-weight: 500;
+  color: inherit;
+}
+.header__link.actived {
+  text-decoration: underline;
+}
+.header__link:hover {
+  text-decoration: underline;
+}
+.toggle-dark {
+  margin-left: 2rem;
+  font-size: 1.5rem;
+}
+.header__toggler .fas,
+.toggle-dark .fas {
+  color: var(--txt-clr);
+}
+@media screen and (max-width: 768px) {
+  .header__toggler {
+    display: block;
+    z-index: 1;
+  }
+  .header__menu {
+    flex-direction: column;
+    width: 100%;
+    max-width: var(--menu-width);
+    height: 100vh;
+    padding: 1rem;
+    position: absolute;
+    top: 0;
+    right: calc(var(--menu-width) * -1);
+    background-color: var(--body-clr);
+    box-shadow: -.1rem 0 .5rem rgb(25 25 25 / .15);
+    transition: transform var(--duration), background-color var(--duration);
+  }
+  .header__item:first-child,
+  .header__item:last-child {
+    margin-top: auto;
+  }
+  .header__item {
+    margin: .5em 0;
+  }
+  .header__link {
+    padding: .5em;
+  }
+  .toggle-dark {
+    margin-left: 0;
+  }
+}
+@media screen and (max-width: 360px) {
+  .header {
+    padding: .5rem 2rem;
+  }
+  .header__brand {
+    max-width: 4rem;
+    max-height: 4rem;
+  }
+}
+</style>
